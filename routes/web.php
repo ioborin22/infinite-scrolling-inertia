4087fc528a47ed40b7be9a25047f1ee8d1f53a24
ioborin22 <ioborin22@gmail.com>
2024-02-20 15:48:55 +0700

    20240220

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('posts', PostController::class);
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', PostController::class);
 
 Route::middleware([
     'auth:sanctum',
