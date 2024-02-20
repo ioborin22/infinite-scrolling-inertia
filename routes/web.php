edd8af19f7740629f308e7ae616c6c997135ecd5
ioborin22 <ioborin22@gmail.com>
2024-02-20 15:49:14 +0700

    20240220

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,15 @@ use Inertia\Inertia;
 
 Route::get('/', PostController::class);
 
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
